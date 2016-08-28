4ea16b15a95923478c284bd4ea84a6059c9e1742
mtslck <mats.loock@lnu.se>
2016-08-28 13:10:14 +0200

    Initial commit

---
+++ src/abacus.js
@@ -0,0 +1,22 @@
+/**
+ * Module for a very simple adding machine.
+ *
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Returns the sum of the arguments passed.
+ *
+ * @param {...*} var_args
+ * @returns {Number} The sum of an arbitrary number of numbers.
+ */
+exports.add = function(var_args) {
+  // Utilize the 'arguments' object here, not the 'var_args' parameter.
+
+  // TODO: Write your code here.
+
+};
